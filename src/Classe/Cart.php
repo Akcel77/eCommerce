b3feb446954e0e6205eba8458a91df9a7f380b8e
Diagne Axel <axel.diagne2021@campus-eni.fr>
2022-05-28 16:56:59 +0200

    (+ / - / delete) product from cart

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -2,19 +2,24 @@
 
 namespace App\Classe;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Cart
 {
     private $session;
+    private $entityManager;
 
     /**
      * Cart constructor.
-     * @param $session
+     * @param SessionInterface $session
+     * @param EntityManagerInterface $entityManager
      */
-    public function __construct(SessionInterface $session)
+    public function __construct(SessionInterface $session, EntityManagerInterface $entityManager)
     {
         $this->session = $session;
+        $this->entityManager = $entityManager;
     }
 
     public function add($id)
@@ -39,4 +44,49 @@ class Cart
     {
         return $this->session->remove('cart');
     }
+
+    public function delete($id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        unset($cart[$id]);
+
+        return $this->session->set('cart', $cart);
+    }
+
+    public function decrease($id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        if($cart[$id] > 1){
+            $cart[$id]--;
+        }else{
+            unset($cart[$id]);
+        }
+        return $this->session->set('cart', $cart);
+    }
+
+    public function getFull()
+    {
+        $cartComplete = [];
+
+        if ($this->get()){
+            foreach ($this->get() as $id => $quantity) {
+                $productObject = $this->entityManager->getRepository(Product::class)->findOneById($id);
+
+                if (!$productObject){
+                    $this->delete($id);
+                    continue;
+                }
+                $cartComplete[] = [
+                    'product' => $productObject,
+                    'quantity' => $quantity,
+                ];
+            }
+        }
+
+        return $cartComplete;
+    }
+
+
 }
