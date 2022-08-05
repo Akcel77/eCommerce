631758e9ebcd0a10dabf854fc7f83df23a042f2d
Akcel77 <Akeucel@gmail.com>
2022-08-05 03:00:17 +0200

    setStripeSessionId to controller and return success or cancel html

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Order;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Stripe\Checkout\Session;
 use Stripe\Stripe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,30 +15,51 @@ use Symfony\Component\Routing\Annotation\Route;
 class StripeController extends AbstractController
 {
     /**
-     * @Route("/commande/create-session", name="stripe_create_session")
+     * @Route("/commande/create-session/{reference}", name="stripe_create_session")
      */
-    public function index(Cart $cart)
+    public function index(EntityManagerInterface $entityManager, Cart $cart, $reference)
     {
         $product_for_stripe = [];
         $YOUR_DOMAIN = 'http://127.0.0.1:8000';
 
-        foreach ($cart->getFull() as $product) {
+        $order = $entityManager->getRepository(Order::class)->findOneBy(['reference' => $reference]);
+
+        if(!$order){
+            return $this->redirectToRoute('order');
+        }
+
+
+        foreach ($order->getOrderDetails()->getValues() as $product) {
+            $product_object = $entityManager->getRepository(Product::class)->findOneBy(['name' => $product->getProduct()]);
             $product_for_stripe[] = [
                 'price_data' => [
                     'currency' => 'eur',
-                    'unit_amount' => $product['product']->getPrice(),
+                    'unit_amount' => $product->getPrice(),
                     'product_data' => [
-                        'name' => $product['product']->getName(),
-                        'images' => [$YOUR_DOMAIN."/uploads/".$product['product']->getIllustration()],
+                        'name' => $product->getProduct(),
+                        'images' => [$YOUR_DOMAIN."/uploads/".$product_object->getIllustration()],
                     ],
                 ],
-                'quantity' => $product['quantity'],
+                'quantity' => $product->getQuantity(),
             ];
         }
+
+        $product_for_stripe[] = [
+            'price_data' => [
+                'currency' => 'eur',
+                'unit_amount' => $order->getCarrierPrice(),
+                'product_data' => [
+                    'name' => $order->getCarrierName(),
+                    'images' => [$YOUR_DOMAIN],
+                ],
+            ],
+            'quantity' => 1,
+        ];
 
         Stripe::setApiKey('sk_test_51LG57CEVG9yfXzpI8HwOcrrAjHVh6gX6UKmn1k9otYwdm7ftZLaAzpt94Sjr0cwznt3GWD91btG1pWWyGg5jbKr400B5tIoPZi');
 
         $checkout_session = Session::create([
+            'customer_email' => $this->getUser()->getEmail(),
             'line_items' => [
                 $product_for_stripe
             ],
@@ -43,9 +67,12 @@ class StripeController extends AbstractController
                 'card',
             ],
             'mode' => 'payment',
-            'success_url' => $YOUR_DOMAIN . '/success.html',
-            'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+            'success_url' => $YOUR_DOMAIN . '/commande/merci/{CHECKOUT_SESSION_ID}',
+            'cancel_url' => $YOUR_DOMAIN . '/commande/erreur/{CHECKOUT_SESSION_ID}',
         ]);
+
+        $order->setStripeSessionId($checkout_session->id);
+        $entityManager->flush();
 
         return $this->redirect($checkout_session->url);
     }
