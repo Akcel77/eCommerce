6377293e7a1e5b5415aded3ecfd0f85e35622915
Akcel77 <Akeucel@gmail.com>
2022-08-08 15:40:34 +0200

    Set sender mail if success register

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Mail;
 use App\Entity\User;
 use App\Form\RegisterType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
-class RegisterController extends AbstractController
+class  RegisterController extends AbstractController
 {
     private $entityManager;
 
@@ -26,6 +27,7 @@ class RegisterController extends AbstractController
      */
     public function index(Request $request, UserPasswordHasherInterface $encoder): Response
     {
+        $notification = null;
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
 
@@ -34,16 +36,30 @@ class RegisterController extends AbstractController
         if($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
 
-            $psw = $encoder->hashPassword($user, $user->getPassword());
-            $user->setPassword($psw);
+            $search_email = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]);
 
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
+            if ($search_email) {
+                $notification = "Cette adresse email est déjà utilisée";
+            } else {
+                $psw = $encoder->hashPassword($user, $user->getPassword());
+                $user->setPassword($psw);
+
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+
+                $mail = new Mail();
+                $content = "Bonjour ".$user->getFirstName()." <br> Bienvenue sur le nom du site lorem ipsum dolor sit amet, consectetur adipiscing elit. <br>";
+                $mail->send($user->getEmail(), $user->getUsername(), 'Bienvenue sur le site "Mon site"', $content);
+
+                $notification = 'Votre compte a bien été créé !';
+            }
+
 
         }
 
         return $this->render('register/index.html.twig', [
             'form' => $form->createView(),
+            'notification' => $notification,
         ]);
     }
 }
