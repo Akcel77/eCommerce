646eefc0ddd58eb629e5aeea6f54218d73f26713
Akcel77 <Akeucel@gmail.com>
2022-08-10 01:12:09 +0200

    Reset password type

---
+++ src/Form/ResetPasswordType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class ResetPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('new_password',RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent etre identique',
+                'constraints' => new Length([
+                    'min' => 8,
+                    'max' => 30,
+                ]),
+                'label' => 'Votre nouveau mot de passe :',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mon nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de saisir votre nouveau mot de passe',
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer votre nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de confirmer votre nouveau mot de passe',
+                    ]
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Mettre à jour mon mot de passe',
+                'attr' => [
+                    'class' => 'btn btn-info',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
