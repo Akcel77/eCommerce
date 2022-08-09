764f0449c37ab8ad9bc5f37aed2c23e883d88344
Akcel77 <Akeucel@gmail.com>
2022-08-09 02:39:33 +0200

    Config State in adminOrderCrudController

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -2,20 +2,38 @@
 
 namespace App\Controller\Admin;
 
+use App\Classe\Mail;
 use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 
 class OrderCrudController extends AbstractCrudController
 {
+    private $entityManager;
+    private $crudUrlGenerator;
+
+    public function __construct(EntityManagerInterface $entityManager, CrudUrlGenerator $crudUrlGenerator)
+    {
+        $this->entityManager = $entityManager;
+        $this->crudUrlGenerator = $crudUrlGenerator;
+    }
+
+
     public static function getEntityFqcn(): string
     {
         return Order::class;
@@ -23,8 +41,45 @@ class OrderCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
+
+        $updatePreparation = Action::new('updatePreparation', 'Préparation en cours', 'fas fa-box-open')->linkToCrudAction('updatePreparation');
+        $updateDelivery = Action::new('updateDelivery', 'Livraison en cours', 'fas fa-truck')->linkToCrudAction('updateDelivery');
         return $actions
+            ->add('detail', $updatePreparation)
+            ->add('detail', $updateDelivery)
             ->add('index', 'detail');
+    }
+
+    public function updatePreparation(AdminContext $context)
+    {
+        $order = $context->getEntity()->getInstance();
+        $order->setState(2);
+        $this->entityManager->flush();
+
+        $this->addFlash('notice', '<span style="color: green;">La commande' . $order->getReference() . ' est bien en cours de préparation</span>');
+
+        $routeBuilder = $this->get(AdminUrlGenerator::class);
+
+        return $this->redirect($routeBuilder->setController(OrderCrudController::class)->setAction('index')->generateUrl());
+    }
+
+    public function updateDelivery(AdminContext $context)
+    {
+        $order = $context->getEntity()->getInstance();
+        $order->setState(3);
+        $this->entityManager->flush();
+
+        $this->addFlash('notice', '<span style="color: orange;">La commande' . $order->getReference() . ' est bien en cours de livraison</span>');
+
+        $routeBuilder = $this->get(AdminUrlGenerator::class);
+
+
+
+        $mail = new Mail();
+        $content = "<br> Bonjour " .$order->getUser()->getFirstName(). " <br> Votre commande est en cours de livraison <br>";
+        $mail->send($order->getUser()->getEmail(), $order->getUser()->getFirstName(), 'Votre commande est en cours de livraison', $content);
+
+        return $this->redirect($routeBuilder->setController(OrderCrudController::class)->setAction('index')->generateUrl());
     }
 
     public function configureCrud(Crud $crud): Crud
@@ -39,10 +94,16 @@ class OrderCrudController extends AbstractCrudController
             IdField::new('id'),
             DateTimeField::new('createdAt', 'Passée le'),
             TextField::new('user.getFullName', 'Utilisateur '),
+            TextEditorField::new('delivery', 'Adresse de livraison')->formatValue(function ($value) { return $value; })->onlyOnDetail(),
             MoneyField::new('total', 'Total produit')->setCurrency('EUR'),
             TextField::new('carrierName', 'Transporteur'),
             MoneyField::new('carrierPrice', 'Frais de port')->setCurrency('EUR'),
-            BooleanField::new('isPaid', 'Payée'),
+            ChoiceField::new('state', 'status')->setChoices([
+                'Non payée' => '0',
+                'Payée' => '1',
+                'Préparation en cours' => '2',
+                'Livraison en cours' => '3',
+            ]),
             ArrayField::new('orderDetails', 'Produits')->hideOnIndex(),
         ];
     }
